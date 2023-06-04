ae0d9d755e98c2d81f3f1ddb73a5c5b74acda2f4
IgorMichels <igorxadrezbr@gmail.com>
2023-06-03 22:25:27 -0300

    update analysis

--- analysis.py
+++ analysis.py
@@ -10,92 +10,90 @@ import sys
 import os
 
 if __name__ == '__main__':
-    t = 0
     slice_point = 0
-    oversampled = 0
     for arg in sys.argv:
-        if '-t' in arg: t = int(arg.split('=')[-1])
         if '-s' in arg: slice_point = float(arg.split('=')[-1])
-        if '-o' in arg: oversampled = int(arg.split('=')[-1])
-
-    X_data, y_data = load_data('validation')
-    y_data = y_data[:, t]
-    clfs = glob(f'models/*Classifier*t{t}s{slice_point}o{oversampled}*')
-    regs = sorted(glob(f'models/*Regressor*t{t}s{slice_point}*o{oversampled}*'))
-
-    clfs_names = [clf.split('/')[1].split('Classifier')[0] for clf in clfs]
-    regs_names = [reg.split('/')[1].split('Regressor')[0] for reg in regs]
-
-    clfs = [load(open(clf, 'rb')) for clf in clfs]
-    regs = [load(open(reg, 'rb')) for reg in regs]
-
-    clfs_results = [clf.predict_proba(X_data) for clf in clfs]
-    clfs_results = [[result, np.argmax(result, axis = 1)] for result in clfs_results]
-    regs_results = [reg.predict(X_data) for reg in regs]
-
-    df = pd.DataFrame(columns = ['target', 'slice_point', 'oversampled',
-                                 'clf', 'reg0', 'reg1', 'reg2',
-                                 'reg0_is_post', 'reg1_is_post', 'reg2_is_post',
-                                 'mae', 'amae'])
     
-    for form in product([0, 1], # clf
-                        [0, 3], # reg0
-                        [1, 4], # reg1
-                        [2, 5], # reg2
-                        [0, 1], # class or prob reg0
-                        [0, 1], # class or prob reg1
-                        [0, 1]  # class or prob reg2
-                       ):
-        
-        clf, reg0, reg1, reg2, m0, m1, m2 = form
+    for t in range(4):
+        for oversampled in [0, 1]:
+            X_data, y_data = load_data('validation')
+            y_data = y_data[:, t]
+            clfs = glob(f'models/*Classifier*t{t}s{slice_point}o{oversampled}*')
+            regs = sorted(glob(f'models/*Regressor*t{t}s{slice_point}*o{oversampled}*'))
 
-        # class 0 prediction
-        inx = clfs_results[clf][1] == 0
-        y_obs = y_data[inx]
-        if m0 == 0: y_hat = regs_results[reg0][inx]
-        else: y_hat = regs_results[reg0][inx] * clfs_results[clf][0][inx, 0] + \
-                      regs_results[reg1][inx] * clfs_results[clf][0][inx, 1] + \
-                      regs_results[reg2][inx] * clfs_results[clf][0][inx, 2]
-        
-        # class 1 prediction
-        inx = clfs_results[clf][1] == 1
-        y_obs = np.hstack([y_obs, y_data[inx]])
-        if m1 == 0: y_aux = regs_results[reg0][inx]
-        else: y_aux = regs_results[reg0][inx] * clfs_results[clf][0][inx, 0] + \
-                      regs_results[reg1][inx] * clfs_results[clf][0][inx, 1] + \
-                      regs_results[reg2][inx] * clfs_results[clf][0][inx, 2]
-        
-        y_hat = np.hstack([y_hat, y_aux])
-        
-        # class 2 prediction
-        inx = clfs_results[clf][1] == 2
-        y_obs = np.hstack([y_obs, y_data[inx]])
-        if m2 == 0: y_aux = regs_results[reg0][inx]
-        else: y_aux = regs_results[reg0][inx] * clfs_results[clf][0][inx, 0] + \
-                      regs_results[reg1][inx] * clfs_results[clf][0][inx, 1] + \
-                      regs_results[reg2][inx] * clfs_results[clf][0][inx, 2]
-        
-        y_hat = np.hstack([y_hat, y_aux])
+            clfs_names = [clf.split('/')[1].split('Classifier')[0] for clf in clfs]
+            regs_names = [reg.split('/')[1].split('Regressor')[0] for reg in regs]
 
-        final_mae = MAE(y_hat, y_obs)
-        final_amae = AMAE(y_obs, y_hat, points = 1000, show = False)
+            clfs = [load(open(clf, 'rb')) for clf in clfs]
+            regs = [load(open(reg, 'rb')) for reg in regs]
 
-        row = [
-            t,
-            slice_point,
-            oversampled == 1,
-            clfs_names[clf],
-            regs_names[reg0],
-            regs_names[reg1],
-            regs_names[reg2],
-            m0 == 1,
-            m1 == 1,
-            m2 == 1,
-            final_mae,
-            final_amae
-        ]
+            clfs_results = [clf.predict_proba(X_data) for clf in clfs]
+            clfs_results = [[result, np.argmax(result, axis = 1)] for result in clfs_results]
+            regs_results = [reg.predict(X_data) for reg in regs]
 
-        df.loc[len(df)] = row
+            df = pd.DataFrame(columns = ['target', 'slice_point', 'oversampled',
+                                         'clf', 'reg0', 'reg1', 'reg2',
+                                         'reg0_is_post', 'reg1_is_post', 'reg2_is_post',
+                                         'mae', 'amae'])
+            
+            for form in product([0, 1], # clf
+                                [0, 3], # reg0
+                                [1, 4], # reg1
+                                [2, 5], # reg2
+                                [0, 1], # class or prob reg0
+                                [0, 1], # class or prob reg1
+                                [0, 1]  # class or prob reg2
+                            ):
+                
+                clf, reg0, reg1, reg2, m0, m1, m2 = form
 
-    if 'results' not in os.listdir(): os.mkdir('results')
-    df.to_csv(f'results/t{t}s{slice_point}o{oversampled}.csv', index = False)
+                # class 0 prediction
+                inx = clfs_results[clf][1] == 0
+                y_obs = y_data[inx]
+                if m0 == 0: y_hat = regs_results[reg0][inx]
+                else: y_hat = regs_results[reg0][inx] * clfs_results[clf][0][inx, 0] + \
+                              regs_results[reg1][inx] * clfs_results[clf][0][inx, 1] + \
+                              regs_results[reg2][inx] * clfs_results[clf][0][inx, 2]
+                
+                # class 1 prediction
+                inx = clfs_results[clf][1] == 1
+                y_obs = np.hstack([y_obs, y_data[inx]])
+                if m1 == 0: y_aux = regs_results[reg0][inx]
+                else: y_aux = regs_results[reg0][inx] * clfs_results[clf][0][inx, 0] + \
+                              regs_results[reg1][inx] * clfs_results[clf][0][inx, 1] + \
+                              regs_results[reg2][inx] * clfs_results[clf][0][inx, 2]
+                
+                y_hat = np.hstack([y_hat, y_aux])
+                
+                # class 2 prediction
+                inx = clfs_results[clf][1] == 2
+                y_obs = np.hstack([y_obs, y_data[inx]])
+                if m2 == 0: y_aux = regs_results[reg0][inx]
+                else: y_aux = regs_results[reg0][inx] * clfs_results[clf][0][inx, 0] + \
+                              regs_results[reg1][inx] * clfs_results[clf][0][inx, 1] + \
+                              regs_results[reg2][inx] * clfs_results[clf][0][inx, 2]
+                
+                y_hat = np.hstack([y_hat, y_aux])
+
+                final_mae = MAE(y_hat, y_obs)
+                final_amae = AMAE(y_obs, y_hat, points = 1000, show = False)
+
+                row = [
+                    t,
+                    slice_point,
+                    oversampled == 1,
+                    clfs_names[clf],
+                    regs_names[reg0],
+                    regs_names[reg1],
+                    regs_names[reg2],
+                    m0 == 1,
+                    m1 == 1,
+                    m2 == 1,
+                    final_mae,
+                    final_amae
+                ]
+
+                df.loc[len(df)] = row
+
+            if 'results' not in os.listdir(): os.mkdir('results')
+            df.to_csv(f'results/t{t}s{slice_point}o{oversampled}.csv', index = False)
--- utils.py
+++ utils.py
@@ -1,22 +1,15 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 def MAE(y_pred, y_obs):
     return np.mean(np.abs(y_pred - y_obs))
 
-def AMAE(y_obs, y_pred, points = 1000, show = True):
+def AMAE(y_obs, y_pred, points = 1000):
     limits = np.linspace(0, 100, points)
     dx = limits[1] - limits[0]
     maes = list()
     for x in limits:
         inx = y_obs >= x
         maes.append(MAE(y_pred[inx], y_obs[inx]))
-
-    if show:
-        plt.plot(limits, maes)
-        plt.xlabel('Threshold')
-        plt.ylabel('MAE')
-        plt.show()
 
     return np.sum(maes) * dx
 
@@ -31,4 +24,4 @@ if __name__ == '__main__':
     y_pred = y_pred * 100
     y_obs = y_obs / np.max(y_obs) * 100
 
-    print(AMAE(y_obs, y_pred, show = True))
+    print(AMAE(y_obs, y_pred))
